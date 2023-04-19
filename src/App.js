5d03fb6a22d30e50ae46ed60ef9399f3693fe0ab
vatari <majestic13@gmail.com>
2023-04-19 09:30:42 +0300

    passed data to List component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <main>
       <section className="container">
-        <h3>0 birthdays today</h3>
+        <h3>{people.length} birthdays today</h3>
         <List people={people} />
         <button onClick={() => console.log("clicked")}>Clear all</button>
       </section>
